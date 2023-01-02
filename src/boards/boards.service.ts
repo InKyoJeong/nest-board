233bca14519df2ba514ad4dc265f0d0fd44d8cc7
InKyoJeong <viapolar@naver.com>
2023-01-02 23:19:39 +0900

    feat: add validation find board, delete board, custom pipe

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { v1 as uuid } from 'uuid';
 import { Board, BoardStatus } from './board.model';
 import { CreateBoardDto } from './dto/create-board.dto';
@@ -26,11 +26,18 @@ export class BoardsService {
   }
 
   getBoardById(id: string): Board {
-    return this.boards.find((board) => board.id === id);
+    const foundBoard = this.boards.find((board) => board.id === id);
+
+    if (!foundBoard) {
+      throw new NotFoundException(`Can't find Board with id ${id}`);
+    }
+
+    return foundBoard;
   }
 
   deleteBoard(id: string): void {
-    this.boards = this.boards.filter((board) => board.id !== id);
+    const foundBoard = this.getBoardById(id);
+    this.boards = this.boards.filter((board) => board.id !== foundBoard.id);
   }
 
   updateBoardStatus(id: string, status: BoardStatus): Board {
