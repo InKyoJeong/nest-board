6eb9a1deb499a3a7ad4cc6352505b9ba9994f293
InKyoJeong <viapolar@naver.com>
2023-01-03 21:42:54 +0900

    feat: add auth guard to board

--- src/boards/boards.module.ts
+++ src/boards/boards.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from 'src/auth/auth.module';
 
 import { TypeOrmExModule } from 'src/typeorm-ex/typeorm-ex.module';
 import { BoardRepository } from './board.repository';
@@ -6,7 +7,7 @@ import { BoardsController } from './boards.controller';
 import { BoardsService } from './boards.service';
 
 @Module({
-  imports: [TypeOrmExModule.forCustomRepository([BoardRepository])],
+  imports: [TypeOrmExModule.forCustomRepository([BoardRepository]), AuthModule],
   controllers: [BoardsController],
   providers: [BoardsService],
 })
